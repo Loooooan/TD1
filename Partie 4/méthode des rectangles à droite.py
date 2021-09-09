c620b20882e001a1c076d703f3700107c221968f
Loooooan <84080826+Loooooan@users.noreply.github.com>
2021-09-09 17:40:53 +0200

    Update méthode des rectangles à droite.py

--- Partie 4/méthode des rectangles à droite.py
+++ Partie 4/méthode des rectangles à droite.py
@@ -1,6 +1,16 @@
+import scipy.integrate as sc
+
+def f(x):
+    return x**2
+
 def rectdroite(f, a, b, n):
     res = 0
     h = (b-a)/n
     for i in range(n):
         res += h*f(a+i*h)
     return res
+
+print(rectdroite(f, 0, 1, 500))
+#Affiche rectdroite(f, 0, 1, 500)
+print(sc.quad(f, 0, 1))
+#Affiche (0.33333333333333337, 3.700743415417189e-15)
